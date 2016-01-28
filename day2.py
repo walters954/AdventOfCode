215df6dbaf717cd119038713043ef07e14384e3f
walters954 <wwalt010@fiu.edu>
2016-01-27 23:28:01 -0500

    remade day2 part 1
    had to remake because of audio issues nothing out of the ordinary still
    works perfect

--- day2.py
+++ day2.py
@@ -1,29 +1,28 @@
 __author__ = 'Walters954'
 
 totalPaper = 0
-dimensions = open("day2input.txt")
 
-for line in dimensions:
-    lwhArray = line.split('x')
-    #print(lwhArray)
-    lwhArray[2] = lwhArray[2].replace('\n','')
-    #print(lwhArray)
-    lwhArray = list(map(int,lwhArray))
+dim = open('day2input')
+#print(dim)
 
-    l = lwhArray[0]
-    w = lwhArray[1]
-    h = lwhArray[2]
+for line in dim:
+    #print(line)
+    lwhList = line.split('x')
+    #print(lwhList)
+    lwhList[2] = lwhList[2].replace('\n','')
+    #print(lwhList)
+    lwhList = list(map(int,lwhList))
 
-    surfArea = (2*l*w + 2*w*h + 2*h*l)
-    #print(surfArea)
-    lwhArray.sort()
-    #print(lwhArray)
-    smallArea = lwhArray[0] * lwhArray[1]
-    #print(smallArea)
+    l = lwhList[0]
+    w = lwhList[1]
+    h = lwhList[2]
+
+    surfArea = 2*l*w + 2*w*h + 2*h*l
+    lwhList.sort()
+    #print(lwhList)
+    smallArea = lwhList[0] * lwhList[1]
     totalPaper += smallArea + surfArea
+
 print(totalPaper)
 print('1586300')
-dimensions.close()
-
-
-
+dim.close()
