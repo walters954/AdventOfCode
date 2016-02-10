d4d4e6a29984d99f2a755571a8a25a7e910c5d3d
walters954 <wwalt010@fiu.edu>
2016-02-10 17:13:33 -0500

    day 4 p 1 & 2

---
+++ day4.py
@@ -0,0 +1,19 @@
+__author__ = 'Warren'
+
+import hashlib
+
+key_start = 'ckczppom'
+
+for num in range(1000000):
+    digest = hashlib.md5((key_start + str(num)).encode())
+    result = digest.hexdigest()
+    if result[:5] == '00000':
+        print(result)
+        print((key_start + str(num)))
+
+for num in range(10000000):
+    digest = hashlib.md5((key_start + str(num)).encode())
+    result = digest.hexdigest()
+    if result[:6] == '000000':
+        print(result)
+        print((key_start + str(num)))
